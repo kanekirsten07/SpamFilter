f3464c282c7d9ae7c0b9fa5279e5b14d17978606
Kirsten Kane <kanekirsten07@gmail.com>
2012-09-21 15:59:19 -0700

    Added Flag

--- src/com/samuraichikx/spamfilter/Emails.java
+++ src/com/samuraichikx/spamfilter/Emails.java
@@ -6,12 +6,17 @@ public class Emails {
 	
 	private int value, x, y;
 	private Bitmap emailtype;
+	private boolean flagged = false;
 	public Emails(int scorevalue, int xcoord, int ycoord, Bitmap etype)
 	{
 		this.emailtype = etype;
 		this.value= scorevalue;
 		this.x = xcoord;
 		this.y = ycoord;
+	}
+	public void flag()
+	{
+		!flagged;
 	}
 	
 	public int x()
