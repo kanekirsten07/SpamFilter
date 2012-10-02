3f561f6869818ba72a9266965949f96367a1702f
Kirsten Kane <kanekirsten07@gmail.com>
2012-10-02 08:56:11 -0700

    Menu Icon Change

--- gen/com/samuraichikx/spamfilter/R.java
+++ gen/com/samuraichikx/spamfilter/R.java
@@ -28,11 +28,16 @@ public final class R {
         public static final int bubble=0x7f020000;
         public static final int envelope=0x7f020001;
         public static final int envelope2=0x7f020002;
-        public static final int ic_action_search=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int shredderguy1=0x7f020005;
-        public static final int shredderguy2=0x7f020006;
-        public static final int shredderguy3=0x7f020007;
+        public static final int envelope3=0x7f020003;
+        public static final int envelope4=0x7f020004;
+        public static final int howto=0x7f020005;
+        public static final int ic_action_search=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int newgame=0x7f020008;
+        public static final int options=0x7f020009;
+        public static final int shredderguy1=0x7f02000a;
+        public static final int shredderguy2=0x7f02000b;
+        public static final int shredderguy3=0x7f02000c;
     }
     public static final class id {
         public static final int game_board_view=0x7f0b0000;
